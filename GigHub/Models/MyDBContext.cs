c35d7d22a77d19406f363f43f7fea73a408f1c77
Abdelwahed Abdelmageed <abdelwahed311@hotmail.com>
2022-01-09 18:22:46 +0200

    First Migration.

---
+++ GigHub/Models/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GigHub.Models
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+    }
+}
