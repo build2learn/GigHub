951d68f976e0a32a1cb3008a0cb332a00049bbd7
Abdelwahed Abdelmageed <abdelwahed311@hotmail.com>
2022-01-11 18:31:50 +0200

    Create Domain Classes Enable Migration Add & update database

---
+++ GigHub/Migrations/202201111612579_CreateGigTable.cs
@@ -0,0 +1,17 @@
+﻿namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateGigTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("CREATE TABLE[dbo].[Gig]([GigID][uniqueidentifier] NOT NULL, [GigStatus][int] NOT NULL");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ GigHub/Migrations/202201111629558_CreateNewTable.cs
@@ -0,0 +1,17 @@
+﻿namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateNewTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("CREATE TABLE[dbo].[Gig]([GigID][uniqueidentifier] NOT NULL, [GigStatus][int]  NULL)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
