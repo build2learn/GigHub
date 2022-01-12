0b48d875f6b1973373b4db8840b1276c9e27a187
Abdelwahed Abdelmageed <abdelwahed311@hotmail.com>
2022-01-12 17:35:20 +0200

    Anti forgery Token (SQL-Injection ,XSS,and CSRF)

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
